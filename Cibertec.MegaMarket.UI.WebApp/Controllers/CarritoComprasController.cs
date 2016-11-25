0bba448a5483f8275dd66b4dd16cdc61801d1cf8
jruiz <jruiz@JRUIZ_WS>
2016-11-25 18:43:26 -0500

    Cambios

---
+++ Cibertec.MegaMarket.UI.WebApp/Controllers/CarritoComprasController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cibertec.MegaMarket.UI.WebApp.Controllers
+{
+    public class CarritoComprasController : Controller
+    {
+        // GET: CarritoCompras
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CarritoCompras/AdicionarAlCarrito/5
+        public ActionResult AdicionarAlCarrito(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Cibertec.MegaMarket.UI.WebApp/Models/CarritoCompras.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cibertec.MegaMarket.UI.WebApp.Models
+{
+    public class CarritoCompras
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal PrecioTotal { get; set; }
+    }
+}
